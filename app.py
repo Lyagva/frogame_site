bd5296a9b9b3fd3b831df927dbd59a3a96cf784b
Lyagva <githublyagva@gmail.com>
2023-09-28 23:13:42 +0300

    getFile testing

--- app.py
+++ app.py
@@ -2,9 +2,12 @@
 
 import os
 import json
-from flask import Flask, render_template, json, request
+from flask import Flask, render_template, json, request, send_file
 from src import commands
+from pathlib import Path
 
+
+current_path = str(Path(__file__).parent.resolve()) + "/"
 app = Flask(__name__)
 
 
@@ -24,6 +27,10 @@ def get_prompt():
 
     return json.dumps(return_data)
 
+
+@app.route("/getFile/<file>", methods=["GET", "POST"])
+def get_file(file):
+    return send_file(current_path + "src/fileSys/files/" + file)
 
 if __name__ == "__main__":
     commands.updateCommands()
